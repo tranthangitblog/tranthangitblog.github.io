e18e968cf982d0501b62934c72b89a62084c9285
Tran Thang <tranthangit.svip@gmail.com>
2024-07-23 12:44:21 +0700

    Site updated: 2024-07-23 12:44:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/23/hello-world/"];function toRandomPost(){
+var posts=["2024/07/23/hello-world/","2024/07/23/huong-dan-dung-github/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
